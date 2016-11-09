5deab19f6a158d1f436451b9e19546597ce540b9
Danielle <daniellesoldera@gmail.com>
2016-11-09 18:00:41 -0200

    update project

---
+++ app/shared/services/data.service.ts
@@ -0,0 +1,12 @@
+//https://angular.io/docs/ts/latest/api/#!?query=injectable
+
+import { Injectable } from 'angular2/core';
+
+@Injectable()
+
+class DataService {
+
+  getCustomersSummary {
+
+  }
+}
