ae6322f5658895f3424b9762d5ee678c940991ae
Danielle Soldera <dsoldera@its.jnj.com>
2016-11-10 11:24:58 -0200

    update app

---
+++ app/app.routes.ts
@@ -0,0 +1,8 @@
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {}
+];
+
+// Export modulo de rotas do angular.
+export const routing = RouterModule.forRoot(appRoutes);
