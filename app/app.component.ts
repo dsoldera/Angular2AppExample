2c2fec12785e35feb854bc8a06d45f449472f703
Danielle Soldera <dsoldera@its.jnj.com>
2016-11-10 11:28:08 -0200

    update app 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'pm-app',
-    template: `<h1>Hello World</h1>`
+    selector: 'app',
+    template: 'app.component.html'
 })
 export class AppComponent {}
